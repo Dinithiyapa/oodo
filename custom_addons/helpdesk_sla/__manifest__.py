3c66205725d92c4c43e075c543eb1099fe405a88
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-01-13 14:52:17 +0530

    created manifest file including core details and dependencies for the module

---
+++ custom_addons/helpdesk_sla/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'helpdesk_sla',
+    'author': 'WireApps',
+    'version': '1.0',
+    'category': 'Help Desk',
+    'depends': ['base','web'],
+    "data": [
+        "views/sla.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
